d748b26d8d2a198e4b1fc90ca83a1aeca55ea6a5
JakaTan <jakaTanugraha@tamu.edu>
2022-10-07 09:04:18 -0500

    adding seeding to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get 'officer_page', action: :index, controller: 'officer_pages'
-
+  resources :officer_pages
   resources :home_pages
   resources :contact_forms
   resources :companies
