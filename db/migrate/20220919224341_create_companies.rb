64b962ccf42f7527b101133fa26fdde08904de89
David King <davidking@tamu.edu>
2022-10-14 17:48:41 -0500

    fixed migration issue

--- db/migrate/20220919224341_create_companies.rb
+++ db/migrate/20220919224341_create_companies.rb
@@ -2,7 +2,7 @@ class CreateCompanies < ActiveRecord::Migration[6.1]
   def change
     create_table :companies do |t|
       t.string :companyName
-      t.string :website
+      t.string :company_website
 
       t.timestamps
     end
