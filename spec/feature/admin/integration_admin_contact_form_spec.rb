1bbd50153e0a8a040c977d701ddca2e8dbc8ca1e
David King <davidking@tamu.edu>
2022-10-16 21:10:32 -0500

    Added tests and minor fixes

--- spec/feature/admin/integration_admin_contact_form_spec.rb
+++ spec/feature/admin/integration_admin_contact_form_spec.rb
@@ -4,3 +4,36 @@ require 'rails_helper'
 #
 # This tests the ability to perform crud action on 'contact_forms' through admin pages
 #
+RSpec.describe('Admin is able to', type: :feature) do
+     before :all do # Create admin used in mockoauth
+          Admin.create!(email: 'test@test.com')
+     end
+
+     before do # Log in the admin
+          Rails.application.env_config['devise.mapping'] = Devise.mappings[:admin] # If using Devise
+          Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
+          visit 'admins/sign_in'
+          click_on 'Sign in'
+     end
+
+     after do # Log out the admin
+          visit destroy_admin_session_path
+     end
+
+     #
+     # Adding a contact form is done through the officer page
+     #
+
+     #
+     # No one should be able to edit the contact forms
+     #
+
+     it 'delete a contact form' do
+          contact_form = ContactForm.create!(person_id: 'test', email: 'test', name: 'test', message: 'test')
+          visit contact_forms_path
+          expect(page).to(have_content('test'))
+          click_on 'Destroy'
+          expect(page).not_to(have_content('test'))
+     end
+
+end
--- spec/feature/admin/integration_admin_person_spec.rb
+++ spec/feature/admin/integration_admin_person_spec.rb
@@ -4,3 +4,68 @@ require 'rails_helper'
 #
 # This tests the ability to perform crud action on 'people' through admin pages
 #
+RSpec.describe('Admin is able to', type: :feature) do
+     before :all do # Create admin used in mockoauth
+          Admin.create!(email: 'test@test.com')
+     end
+
+     before do # Log in the admin
+          Rails.application.env_config['devise.mapping'] = Devise.mappings[:admin] # If using Devise
+          Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
+          visit 'admins/sign_in'
+          click_on 'Sign in'
+     end
+
+     after do # Log out the admin
+          visit destroy_admin_session_path
+     end
+
+     it 'add a person' do
+          visit people_path
+          click_on 'New Person'
+          fill_in 'person_name', with: 'david'
+          fill_in 'person_class_year', with: 'test'
+          fill_in 'person_membership_length', with: 'test'
+          click_on 'Create Person'
+          expect(:notice).to(be_present)
+          visit people_path
+          expect(page).to(have_content('david'))
+     end
+
+     it 'add a person with bad param' do
+          visit people_path
+          click_on 'New Person'
+          fill_in 'person_name', with: ''
+          fill_in 'person_class_year', with: ''
+          fill_in 'person_membership_length', with: ''
+          click_on 'Create Person'
+          expect(page).to(have_content('prohibited this person from being saved'))
+     end
+
+     it 'update a person' do
+          person = Person.create!(name: 'apple', class_year: 'test', membership_length: 'test')
+          visit edit_person_path(person.id)
+          fill_in 'person_name', with: 'orange'
+          click_on 'Update Person'
+          expect(:notice).to(be_present)
+          visit people_path
+          expect(page).to(have_content('orange'))
+     end
+
+     it 'update a person with bad params' do
+          person = Person.create!(name: 'apple', class_year: 'test', membership_length: 'test')
+          visit edit_person_path(person.id)
+          fill_in 'person_name', with: ''
+          click_on 'Update Person'
+          expect(page).to(have_content('prohibited this person from being saved'))
+     end
+
+     it 'delete a person' do
+          Person.create!(name: 'apple', class_year: 'test', membership_length: 'test')
+          visit people_path
+          expect(page).to(have_content('apple'))
+          click_on 'Destroy'
+          expect(page).not_to(have_content('apple'))
+     end
+
+end
