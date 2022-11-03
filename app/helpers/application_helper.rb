d767e2957853ce257bdd9acb07ecf145968049b9
JakaTan <jakaTanugraha@tamu.edu>
2022-11-03 10:33:51 -0500

    Finished styling for input boxes, and fixed officer cards

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+    def flash_class(level)
+        bootstrap_alert_class = {
+          "success" => "alert-success",
+          "error" => "alert-danger",
+          "notice" => "alert-info",
+          "alert" => "alert-danger",
+          "warn" => "alert-warning"
+        }
+        bootstrap_alert_class[level]
+      end
 end
